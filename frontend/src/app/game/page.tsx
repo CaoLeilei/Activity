4ee4333aecf45e0f8745ce0b414ef2e7badfe0fb
carly.he <ll900618@163.com>
2024-08-07 23:14:28 +0800

    初始化项目

---
+++ frontend/src/app/game/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+import { Layout } from "antd";
+export default function Game()
+{
+  return (
+    <Layout>
+      <Layout.Content>
+        <main>
+          <h1>Hello, Next.js!</h1>
+        </main>
+      </Layout.Content>
+    </Layout>
+  );
+}
